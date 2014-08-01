4ba97f2e6e55ad94c44c1534c386a26da886a9e5
Luke McGovern <lukemcgovern6@gmail.com>
2014-08-01 17:16:39 +0100

    Create Path.java

---
+++ Path.java
@@ -0,0 +1,20 @@
+
+
+public class Path
+{
+    // instance variables - replace the example below with your own
+    public double dist;
+    public int[] path;
+    
+    public Path(int[] x, double y)
+    {
+       path=x;
+       dist=y;
+    }
+
+    
+    public Path(){
+         dist=0.0;
+    }
+    
+}
